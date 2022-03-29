a2710aff3a55b15d81a1cadddd87b8e6ed51e206
hadimsm <st.sbu@yahoo.com>
2022-03-29 10:18:42 +0430

    app: config MongoAdaptor

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import fastify, { FastifyInstance } from "fastify";
 import config from "./config";
+import MongoAdapter from "./adapters/mongo.adapter";
+
 class App {
   public app: FastifyInstance;
   public app_domain: string = config.app.domain;
   public app_port: number = parseInt(`${config.app.port}`, 10) ?? 8080;
 
+  private mongoDatabaseInfo = {
+    username: config.db.mongo.username!,
+    password: config.db.mongo.password!,
+    host: config.db.mongo.host!,
+    port: parseInt(`${config.db.mongo.port}`, 10) ?? 27017,
+    dbName: config.db.mongo.name!,
+    authName: config.db.mongo.auth!,
+  };
+
   constructor(appInit: { plugins: any; routes: any }) {
     this.app = fastify({ logger: true });
-
+    this.connectMongo();
     this.routes(appInit.routes);
+  }
+
+  private async connectMongo() {
+    let { username, password, host, port, dbName, authName } =
+      this.mongoDatabaseInfo;
+    await new MongoAdapter(username, password, host, port, dbName, authName);
   }
 
   public routes(routes: { forEach: (arg0: (routes: any) => void) => void }) {
