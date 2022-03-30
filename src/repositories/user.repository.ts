9445c7a9be6145c545c22be1a10d0e61a83fe070
hadimsm <st.sbu@yahoo.com>
2022-03-30 13:32:13 +0430

    repositry: add userRepository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,41 @@
+import models from "../entities/schemas";
+import { UserInterface } from "../entities/interfaces/user.interface";
+
+class UserRepository {
+  private static instance: UserRepository;
+
+  constructor() {}
+
+  public static getInstance(): UserRepository {
+    if (!UserRepository.instance) {
+      UserRepository.instance = new UserRepository();
+    }
+    return UserRepository.instance;
+  }
+
+  public async findAllUsers(): Promise<any> {
+    const result = await models.Trade.findAll({
+      order: [["id", "ASC"]],
+    });
+    return result;
+    // as TradeInterface[];
+  }
+
+  public createUser(user: UserInterface): Promise<any> {
+    const object = {
+      name: user.name,
+    };
+    if (user.id != undefined) object["id"] = user.id;
+    return models.User.create(object);
+  }
+
+  public getUserById(id: number): Promise<any> {
+    return models.User.findAll({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
+
+export default UserRepository;
