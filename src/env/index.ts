36b31e853b3343d65c0e7fc9e3b4072d54a39590
Hadi Masoumi <st.sbu@yahoo.com>
2022-04-03 07:38:59 +0430

    fix env for linux ubuntu

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,22 +1,6 @@
 import configYaml from "config-yaml";
 const config = configYaml(`${__dirname}/../../config/config.yml`);
 
-// const config = configYaml(`./config/config.yml`);
+console.log("process.env.NODE_ENV >> ", process.env.NODE_ENV);
 
-let NODE_ENV: string;
-switch (parseInt(process.env.NODE_ENV as string)) {
-  case 1:
-    NODE_ENV = "development";
-    break;
-  case 2:
-    NODE_ENV = "production";
-    break;
-  case 3:
-    NODE_ENV = "staging";
-    break;
-  default:
-    NODE_ENV = "development";
-}
-
-console.log("NODE_ENV :>> ", NODE_ENV);
-export default config[NODE_ENV];
+export default config[process.env.NODE_ENV];
